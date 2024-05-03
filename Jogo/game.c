7df8d2ce86f9d46fc0e1c16626dcdffd8ce79971
Sofia-Saraiva <sofia19lima@gmail.com>
2024-05-03 00:22:26 -0300

    menu

--- Jogo/game.c
+++ Jogo/game.c
@@ -1,28 +1,127 @@
+#include <stdio.h>
 #include "raylib.h"
 
-#define width 510
-#define height 480
+#define WIDTH 510
+#define HEIGHT 480
+#define MAX_INPUT_CHARS 10
+
+typedef enum { 
+    START, 
+    RANKING,
+    RANKING_PAGE,
+    GET_NAME,
+    GAME,
+    END
+} ScreenState;
+
+void TextInput(char *inputText, int *charCount);
 
 int main(void)
 {
-    InitWindow(width, height, "Mario Is Missing");
-    Image icon = LoadImage("src/Telas/menu.png");
-    SetWindowIcon(icon);
+    InitWindow(WIDTH, HEIGHT, "Mario Is Missing");
     SetTargetFPS(60); 
 
-    Texture2D background = LoadTexture("src/Telas/menu.png");
+    // Load Menu
+    Texture2D menu = LoadTexture("src/Telas/menu.png");
+    Texture2D start = LoadTexture("src/Telas/start.png");
+    Texture2D ranking = LoadTexture("src/Telas/ranking.png");
+
+    // Load Ranking
+    Texture2D ranking_page = LoadTexture("src/Telas/ranking_page.png");
+
+    // Load Font
+    Font customFont = LoadFont("src/MarioFont.ttf");
+    char inputText[MAX_INPUT_CHARS + 1] = {0};
+    int charCount = 0;
+
+    ScreenState currentScreen = START;
 
     while (!WindowShouldClose())
     {
+        DisableCursor();
         BeginDrawing();
-            ClearBackground(BLACK);
-            DrawTexture(background, 0, 0, WHITE); 
-        EndDrawing();
-    }
+        switch (currentScreen) {
+            case START:
+                if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)) {
+                 currentScreen = RANKING;
+                        break;
+                }
+               
+                if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)) {
+                    currentScreen = RANKING;
+                    break;
+                }
 
-    UnloadTexture(background);
+                DrawTexture(menu, 0, 0, WHITE); 
+                DrawTexture(start, 165, 260, WHITE); 
+                if (IsKeyPressed(KEY_ENTER)) { 
+                    currentScreen = GET_NAME;
+                    UnloadTexture(menu);
+                    UnloadTexture(start);
+                    break;
+                }
+                break;
+            case RANKING:
+                if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)) {
+                 currentScreen = START;
+                        break;
+                }
+               
+                if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)) {
+                    currentScreen = START;
+                    break;
+                }
+
+                DrawTexture(menu, 0, 0, WHITE); 
+                DrawTexture(ranking, 165, 260, WHITE); 
+                if (IsKeyPressed(KEY_ENTER)) { 
+                    currentScreen = RANKING_PAGE;
+                    UnloadTexture(menu);
+                    UnloadTexture(start);
+                    break;
+                }
+                break;
+            case RANKING_PAGE:
+                DrawTexture(ranking_page, 0, 0, WHITE);
+                break;
+            case GET_NAME:
+                ClearBackground(WHITE);
+                TextInput(inputText, &charCount);
+                DrawTextEx(customFont, inputText, (Vector2){10, 40}, 20, 2, BLACK);
+            case GAME:
+
+            case END:
+                
+        }
+        EndDrawing();
+
+    }
 
     CloseWindow();
 
     return 0;
+}
+
+
+void TextInput(char *inputText, int *charCount) {
+    int key = GetKeyPressed();
+    if (key != 0)
+    {
+        if ((key >= 32) && (key <= 125) && (*charCount < MAX_INPUT_CHARS))
+        {
+            inputText[*charCount] = (char)key;
+            (*charCount)++;
+        }
+        else if ((key == KEY_BACKSPACE) && (*charCount > 0))
+        {
+            (*charCount)--;
+            inputText[*charCount] = '\0';
+        }
+        else if (key == KEY_ENTER)
+        {
+            inputText[*charCount] = '\0';
+            *charCount = 0;
+            printf("Texto digitado: %s\n", inputText);
+        }
+    }
 }
